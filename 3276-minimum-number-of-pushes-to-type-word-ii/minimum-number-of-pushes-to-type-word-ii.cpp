f20b7a0b3a2d91f8820b0d2ec806b8ee2fecc2ad
Satyam Prasad Agrawal <satyamprasadagrawal@gmail.com>
2026-07-31 23:31:42 +0530

    Time: 8 ms (61.48%) | Memory: 25.4 MB (72.47%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.length();
+        vector<int> freq(26,0);
+
+        for( char v:word){
+            freq[v -'a']++;
+        }
+        sort(freq.begin(),freq.end(),greater<int>());
+
+        int ans=0;
+      for(int i=0; i<26; i++){
+         ans+=(freq[i]*(i/8+1));
+      }
+      return ans;
+    }
+};
